2c64f39b726854fc846fe95e93c6444648cc8c61
Mike Easter <michael.easter@qg.com>
2017-07-14 14:09:34 -0500

    Fix typo.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.clojars.mike706574/re-frame-spec-interceptor "0.1.0-SNAPSHOT"
-  :description "An spec interceptor for re-frame."
+  :description "A spec interceptor for re-frame."
   :url "http://github.com/mike706574/re-frame-spec-interceptor"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
