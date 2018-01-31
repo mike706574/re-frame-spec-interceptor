0d08642de0c3240e7ebc1c57eb484fccdde61377
EAS7510 <michaeleaster@northwesternmutual.com>
2018-01-31 17:52:11 -0600

    Resurrect.

--- project.clj
+++ project.clj
@@ -1,7 +1,12 @@
-(defproject org.clojars.mike706574/re-frame-spec-interceptor "0.1.0-SNAPSHOT"
+(defproject fun.mike/re-frame-spec-interceptor "0.0.1-SNAPSHOT"
   :description "A spec interceptor for re-frame."
   :url "http://github.com/mike706574/re-frame-spec-interceptor"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
-                 [org.clojure/spec.alpha "0.1.123"]])
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/spec.alpha "0.1.143"]]
+  :profiles {:dev {:source-paths ["dev"]
+                   :target-path "target/dev"
+                   :dependencies [[org.clojure/clojure "1.9.0"]]}}
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]])
