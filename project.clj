0fd63ddd906521adfd9ee28a197315bbf135f208
EAS7510 <michaeleaster@northwesternmutual.com>
2018-01-31 17:52:24 -0600

    Version 0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fun.mike/re-frame-spec-interceptor "0.0.1-SNAPSHOT"
+(defproject fun.mike/re-frame-spec-interceptor "0.0.1"
   :description "A spec interceptor for re-frame."
   :url "http://github.com/mike706574/re-frame-spec-interceptor"
   :license {:name "Eclipse Public License"
